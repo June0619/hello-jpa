d77cc08a1256d630ec755004f2ed3d5db4213357
bp_jwjung <jjw5050@hotmail.com>
2022-04-14 18:38:16 +0900

    JPA 시작하기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,12 +1,30 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        //CODE
+        Member member = new Member();
+
+        member.setId(2L);
+        member.setName("memberB");
+
+        em.persist(member);
+        tx.commit();
+
+        em.close();
+        emf.close();
     }
 
 }
