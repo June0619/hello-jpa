80b5cccb64e8f53426f36ea52f4c64c71aed0b47
June0619 <jjw5050@hotmail.com>
2022-04-16 22:07:24 +0900

    JPA 소개 완료

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -21,11 +22,22 @@ public class JpaMain {
 //            member.setName("memberB");
 //            em.persist(member);
 //
-//            tx.commit();
-
+            /** Member Select One **/
             Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+
+            /** Member Update **/
+//            findMember.setName("Hello Jpa");
+
+            /** Member Select List **/
+            List<Member> memberList
+                    = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            tx.commit();
+
+            for (Member one : memberList) {
+                System.out.println("findMember.id = " + one.getId());
+                System.out.println("findMember.name = " + one.getName());
+            }
 
         } catch(Exception e) {
             tx.rollback();
