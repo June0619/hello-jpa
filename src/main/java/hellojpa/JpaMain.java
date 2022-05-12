579abfffea7cfaea9fa0797e87c89e82b3a1de16
June0619 <jjw5050@hotmail.com>
2022-05-12 22:39:12 +0900

    매핑 정보 상속

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
 
@@ -17,20 +18,29 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
+//            Movie movie = new Movie();
+//
+//            movie.setName("movie");
+//            movie.setPrice(10000);
+//            movie.setDirector("Man");
+//            movie.setActor("girl");
+//
+//            em.persist(movie);
+//
+//            em.flush();
+//            em.clear();
+//
+//            Movie findMovie = em.find(Movie.class, movie.getId());
+//            System.out.println("findMovie = " + findMovie);
 
-            movie.setName("movie");
-            movie.setPrice(10000);
-            movie.setDirector("Man");
-            movie.setActor("girl");
+            Member member = new Member();
+            member.setName("Tes");
+            member.setCreatedBy("ME");
+            member.setCreatedAt(LocalDateTime.now());
+            member.setModifiedBy("YOU");
+            member.setModifiedAt(LocalDateTime.now());
 
-            em.persist(movie);
-
-            em.flush();
-            em.clear();
-
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
