21f7f59d3a7b2774dec7466801e972d8ca056384
June0619 <jjw5050@hotmail.com>
2022-05-18 23:07:51 +0900

    고아 객체

--- src/main/java/hellojpa/excutable/CascadeMain.java
+++ src/main/java/hellojpa/excutable/CascadeMain.java
@@ -1,4 +1,7 @@
-package hellojpa;
+package hellojpa.excutable;
+
+import hellojpa.Child;
+import hellojpa.Parent;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -35,6 +38,28 @@ public class CascadeMain {
             em.persist(parent);
 //            em.persist(child1);
 //            em.persist(child2);
+
+            /**
+             * Parent 객체에 설정한 orphanRemoval 속성으로 인해
+             * 연관관계가 제거되면 Delete 쿼리가 발생한다.
+             *
+             * 주의사항
+             * 영속성 전이와 마찬가지로 참조하는 곳이 하나일때만 사용해야 함
+             *
+             * CASCADE.REMOVE 처럼 사용한다.
+             * **/
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            // 연관관계가 제거되거나 부모가 제거되는 경우 고아객체가 모두 삭제 됨
+//            findParent.getChildList().remove(0);
+            em.remove(parent);
+
+            /**
+             * 영속성 전이와 고아객체 관리를 모두 사용하면 얻는 이점
+             * - 부모 엔티티를 통해 자식 엔티티의 생명 주기를 관리할 수 있음
+             * -> DDD (Domain Driven Design) 의 Aggregate Root 구현에 유용
+             * -> 쉽게 말해 불필요한 Repository , DAO 가 생략될 수 있음
+             * **/
 
             tx.commit();
         } catch (Exception e) {
