9950784367ea0bb0817c2f193b9fe996152958e8
June0619 <jjw5050@hotmail.com>
2022-05-25 22:06:04 +0900

    lecture: 값 타입의 비교 수업내용 정리
    1. 값 타입 객체의 동등비교

--- src/main/java/hellojpa/Address.java
+++ src/main/java/hellojpa/Address.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 /**
  * 임베디드 타입의 장점
@@ -26,6 +27,21 @@ public class Address {
     private String city;
     private String street;
     private String zip;
+
+    // primitive 타입은 == 동일비교가 가능하지만
+    // 값 타입 객체는 동등비교 (equals) 를 적절하게 재정의 해줌으로써 비교해야 함
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zip, address.zip);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zip);
+    }
 
     // 해당 주소가 유효한 주소인지 판별하는 메서드를 다음 같이 도메인 지향적으로 만들 수 있음
 //    public void isValid() {
