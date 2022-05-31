7528252a5569ee504dc3dd869ebf1c2a5428feec
June0619 <jjw5050@hotmail.com>
2022-05-31 23:40:08 +0900

    JPQL 메인 추가

---
+++ src/main/java/hellojpa/excutable/JpqlMain.java
@@ -0,0 +1,36 @@
+package hellojpa.excutable;
+
+import hellojpa.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class JpqlMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+
+            List<Member> resultList = em.createQuery("select m From Member m where m.name like '%kim%'", Member.class)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
